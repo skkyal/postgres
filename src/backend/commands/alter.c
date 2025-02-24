1e5b9e748360ba48bff4a83ceccf68c031c6299f
Shlok Kyal <shlok.kyal.oss@gmail.com>
2025-02-24 15:34:31 +0530

    Selective Invalidation of Cache
    When we alter a publication, add/drop namespace to/from publication
    all the cache for all the tables are invalidated.
    With this patch for the above operationns we will invalidate the
    cache of only the desired tables.

--- src/backend/commands/alter.c
+++ src/backend/commands/alter.c
@@ -400,6 +400,9 @@ ExecRenameStmt(RenameStmt *stmt)
 		case OBJECT_TYPE:
 			return RenameType(stmt);
 
+		case OBJECT_PUBLICATION:
+			return RenamePublication(stmt->subname, stmt->newname);
+
 		case OBJECT_AGGREGATE:
 		case OBJECT_COLLATION:
 		case OBJECT_CONVERSION:
@@ -417,7 +420,6 @@ ExecRenameStmt(RenameStmt *stmt)
 		case OBJECT_TSDICTIONARY:
 		case OBJECT_TSPARSER:
 		case OBJECT_TSTEMPLATE:
-		case OBJECT_PUBLICATION:
 		case OBJECT_SUBSCRIPTION:
 			{
 				ObjectAddress address;
